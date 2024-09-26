2a220fa924fd7726c488971581a7eaab2c75fa01
Abrorjon Asralov <asralov@arizona.edu>
2024-09-26 09:14:41 -0700

    did some UI

--- MyLibrary.java
+++ MyLibrary.java
@@ -1,7 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class MyLibrary
 {
-	// need to implement some methods with constructor
-	
-	// craeting an instance of librarian who will control books
-	Librarian pulatLibrarian = new Librarian(new BooksCollections());
+    public static void main(String[] args)
+    {
+    	// Writing a welcome message
+    	WelcomeMessage wm = new WelcomeMessage();
+    	System.out.println(wm);
+    	System.out.println("If you need to see a set of commands, please enter \"help\"");
+    	System.out.println("If you want to exit the program, please enter \"stop\"");
+
+    	
+    	// craeting an instance of librarian who will control books
+    	Librarian pulatLibrarian = new Librarian(new BooksCollections());
+    
+    	
+    	// commands list
+    	ArrayList<String> commands = new ArrayList<>();
+    	commands.add("search");
+    	commands.add("addBook");
+    	commands.add("setToRead");
+    	commands.add("rate");
+    	commands.add("getBooks");
+    	commands.add("suggestRead");
+    	commands.add("addBooks");
+    	
+    	Scanner sc = new Scanner(System.in);
+    	String userInput = sc.nextLine().toLowerCase();
+    	while (!userInput.equals("stop"))
+    	{
+//    		System.out.println(" I AM IN THE CONTROLLED LOOP");
+    		
+    		if (userInput.equals("help"))
+    		{
+    			System.out.println("Available Commands:");
+    			for (String command: commands)
+    			{
+    				System.out.println(command);
+    			}
+    		}
+    		
+    		
+    		userInput = sc.nextLine().toLowerCase();
+    		
+    		
+    	}
+    	
+    	
+    	sc.close();
+    	
+    }
+
 }
---
+++ WelcomeMessage.java
@@ -0,0 +1,27 @@
+
+public class WelcomeMessage 
+{
+	private String message;
+	public WelcomeMessage()
+	{
+		this.message = generateWelcomeMessage();
+	}
+	
+	
+    public String generateWelcomeMessage() 
+    {
+    	String line1 = "*****************************************************************";
+    	String line2 = "*                                                               *";
+    	String line3 = "*                   Welcome To MyLibrary App!                   *";
+    	String line4 = "*                                                               *";
+    	String line5 = "*                    Your books, Your rules                     *";
+    	String line6 = "*                                                               *";
+    	String line7 = "*****************************************************************";
+    	return line1 + "\n" + line2 + "\n" + line3 + "\n" + line4 + "\n"+line5 + "\n" + line6 + "\n" + line7;
+    }
+    
+    public String toString()
+    {
+    	return this.message;
+    }
+}
